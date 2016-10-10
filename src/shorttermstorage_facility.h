026d1b4cf3d581676549c17cc56c33a0fc43212d
Anthony Scopatz <scopatz@gmail.com>
2016-10-10 15:30:25 -0400

    some fixes

--- src/shorttermstorage_facility.h
+++ src/shorttermstorage_facility.h
@@ -56,7 +56,7 @@ class ShorttermstorageFacility : public cyclus::Facility  {
   virtual void Tock();
 
   std::set<cyclus::RequestPortfolio<cyclus::Material>::Ptr> GetMatlRequests();
-  
+
   void AcceptMatlTrades(const std::vector< std::pair<cyclus::Trade<cyclus::Material>,
                                       cyclus::Material::Ptr> >& responses);
 
@@ -67,7 +67,7 @@ class ShorttermstorageFacility : public cyclus::Facility  {
                       std::vector<std::pair<cyclus::Trade<cyclus::Material>,
                       cyclus::Material::Ptr> >& responses);
 
-  #pragma cyclus var {"tooltip": "Input commodities that the facility consumes."\
+  #pragma cyclus var {"tooltip": "Input commodities that the facility consumes.", \
                       "doc": "Commodities that the storage facility consumes", \
                       "uitype": "incommodity"}
   std::string in_commod;
@@ -77,12 +77,12 @@ class ShorttermstorageFacility : public cyclus::Facility  {
                       "uitype": "outcommodity", \
                       "uilabel": "Discharge Commodity"}
   std::string out_commod;
-  
+
   #pragma cyclus var {"tooltip": "The maximum decay heat limit that the "\
                       "storage facility can accomidate.", \
                       "doc" : "The facility is designed to support a used " \
-                      "fuel with a decay heat per volume of up to this " \
-                      "value", \
+                              "fuel with a decay heat per volume of up to this " \
+                              "value", \
                       "units": "W/m^3", \
                       "uilabel": "Decay Heat Upper Limit"}
   double dec_heat_ulimit;
@@ -90,8 +90,8 @@ class ShorttermstorageFacility : public cyclus::Facility  {
   #pragma cyclus var {"tooltip": "The maximum decay heat limit that the "\
                       "storage facility can accomidate.", \
                       "doc" : "The facility is designed to support a used " \
-                      "fuel with a decay heat per volume of up to this " \
-                      "value", \
+                              "fuel with a decay heat per volume of up to this " \
+                              "value", \
                       "units": "W/m^3", \
                       "uilabel": "Decay Heat Lower Limit", \
                       "default": 0}
@@ -99,14 +99,14 @@ class ShorttermstorageFacility : public cyclus::Facility  {
 
   #pragma cyclus var {"tooltip": "Capacity per timestep", \
                       "doc" : "The amount of material that the facility " \
-                      "can recieve per timestep.", \
+                              "can recieve per timestep.", \
                       "units": "kg", \
                       "uilabel": "Recieving Capacity"}
   double input_capacity;
 
   #pragma cyclus var {"tooltip": "Capacity per timestep", \
                       "doc" : "The amount of material that the facility " \
-                      "can eject per timestep.", \
+                              "can eject per timestep.", \
                       "units": "kg", \
                       "uilabel": "Removal Capacity"}
   double output_capacity;
@@ -117,7 +117,7 @@ class ShorttermstorageFacility : public cyclus::Facility  {
                       "units": "kg", \
                       "uilabel": "Maximum Storage Size"}
   double maximum_storage;
- 
+
  private:
   cyclus::toolkit::ResourceBuff storage_;
 
